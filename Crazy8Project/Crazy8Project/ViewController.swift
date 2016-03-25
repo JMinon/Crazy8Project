b70a1fac49d25f6c0db5f88e1d194118aef35015
Jason-Ray Minon <j.r.minon@gmail.com>
2016-03-24 20:20:18 -0400

    Commit Xcode project to GitHub
    Committed the Xcode project of the Crazy 8 game.

---
+++ Crazy8Project/Crazy8Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Crazy8Project
+//
+//  Created by Jason on 2016-03-24.
+//  Copyright © 2016 Jason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
